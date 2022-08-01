2015fb28d4b3cbb646be1fed1fe611f6478d2dd5
Noah <noahjcoleman@gmail.com>
2022-08-01 09:22:19 -0600

    part 9 complete

--- boardGameApp/src/app/services/game.service.ts
+++ boardGameApp/src/app/services/game.service.ts
@@ -16,7 +16,12 @@ export class GameService {
   searchByName(searchText: string): Observable<Game[]> {
     const alteredText = searchText.replace(/\s/g, '+');
     return this.http.get<Game[]>(`https://api.boardgameatlas.com/api/search?name=${alteredText}&client_id=${environment.boardgameAPI}`).pipe(
-        map(response => response['games'])
+      map(response => response['games'])
     )
-  };
+  }
+  getById(gameId: string): Observable<Game> {
+    return this.http.get<Game>(`https://api.boardgameatlas.com/api/search?ids=${gameId}&client_id=${environment.boardgameAPI}`).pipe(
+      map(response => response['games'])
+    );
+  }
 }
